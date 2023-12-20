8fdc7681a81811fd58b19a2ac456a31450d909f1
Jiae Kim <82318174+KJA000@users.noreply.github.com>
2023-12-21 08:56:54 +0900

    Add files via upload

---
+++ test_O3/CMakeFiles/test_arm_convolve_s8.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_arm_convolve_s8.dir/Unity/TestRunner/unity_test_arm_convolve_s8_runner.c.o"
+  "CMakeFiles/test_arm_convolve_s8.dir/Unity/TestRunner/unity_test_arm_convolve_s8_runner.c.o.d"
+  "CMakeFiles/test_arm_convolve_s8.dir/Unity/unity_test_arm_convolve_s8.c.o"
+  "CMakeFiles/test_arm_convolve_s8.dir/Unity/unity_test_arm_convolve_s8.c.o.d"
+  "test_arm_convolve_s8"
+  "test_arm_convolve_s8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_arm_convolve_s8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
